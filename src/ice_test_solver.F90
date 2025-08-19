4f77d6b0b0933a4a5ca7b194daa6c4bf27f52f0e
jpgaertner <jph.gaertner@gmail.com>
2025-08-19 13:09:05 +0200

    use u_buff, use contiguous pointers, check for edge orientation

--- src/ice_test_solver.F90
+++ src/ice_test_solver.F90
@@ -16,7 +16,7 @@ contains
     type(t_partit), intent(inout), target :: partit
     type(t_mesh)  , intent(in), target :: mesh
     !___________________________________________________________________________
-    real(kind=WP), dimension(:), pointer  :: u_ice, v_ice
+    real(kind=WP), contiguous, dimension(:), pointer  :: u_buff, v_buff
     integer       :: i, i1, i2
     real(kind=WP) :: expected_u, expected_v, diff_u, diff_v
     real(kind=WP) :: x1, x2, y1, y2, x_edge, y_edge
@@ -24,12 +24,12 @@ contains
 #include "associate_mesh_def.h"
 #include "associate_part_ass.h"
 #include "associate_mesh_ass.h"
-    u_ice => ice%uice(:)
-    v_ice => ice%vice(:)
-    
+    u_buff => ice%nc%u_buff
+    v_buff => ice%nc%v_buff
 
-    u_ice = 0.0_WP
-    v_ice = 0.0_WP
+
+    u_buff = 0.0_WP
+    v_buff = 0.0_WP
 
     do i = 1, myDim_edge2D
         i1 = edges(1, i)
@@ -43,12 +43,16 @@ contains
         x_edge = 0.5_wp * (x1 + x2)
         y_edge = 0.5_wp * (y1 + y2)
     
-        u_ice(i) = sin(x_edge) + cos(y_edge)
-        v_ice(i) = sin(x_edge) - cos(y_edge)
+        !u_ice(i) = sin(x_edge) + cos(y_edge)
+        !v_ice(i) = sin(x_edge) - cos(y_edge)
+
+        ! use this one to catch errors in the orientation of edges
+        u_buff(i) = x2 - x1
+        v_buff(i) = y2 - y1
     end do
 
-    !call exchange_edge2D(u_ice, partit)
-    !call exchange_edge2D(v_ice, partit)
+    call exchange_edge2D(u_buff, partit)
+    call exchange_edge2D(v_buff, partit)
 
     do i = myDim_edge2D+1, myDim_edge2D+eDim_edge2D
         i1 = edges(1, i)
@@ -62,11 +66,14 @@ contains
         x_edge = 0.5_wp * (x1 + x2)
         y_edge = 0.5_wp * (y1 + y2)
     
-        expected_u = sin(x_edge) + cos(y_edge)
-        expected_v = sin(x_edge) - cos(y_edge)
+        !expected_u = sin(x_edge) + cos(y_edge)
+        !expected_v = sin(x_edge) - cos(y_edge)
 
-        diff_u = abs(u_ice(i) - expected_u)
-        diff_v = abs(v_ice(i) - expected_v)
+        expected_u = x2 - x1
+        expected_v = y2 - y1
+
+        diff_u = abs(u_buff(i) - expected_u)
+        diff_v = abs(v_buff(i) - expected_v)
 
         if (diff_u > 1e-10_WP .or. diff_v > 1e-10_WP) then
             print *, 'Halo mismatch at edge', i, diff_u, diff_v
